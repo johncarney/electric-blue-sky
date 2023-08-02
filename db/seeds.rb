84166597bf30d43bc40674ae1a8a914052c06c82
John Carney <john@carney.id.au>
2023-08-02 05:07:33 +0000

    Add topic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#
+# Seeds the database with the TTRPG topics from the YAML file.
+#
+module Seeds
+  TTRPG_TOPICS = Pathname("#{__dir__}/seed-data/ttrpg-topics.yml").freeze
+
+  def self.seed
+    Topic.transaction do
+      categories = YAML.load_file(TTRPG_TOPICS, aliases: true).except(".macros")
+      topics = categories.values.reduce(&:merge)
+      topics.each do |name, attrs|
+        topic = TopicBuilder.call(name, attrs.deep_symbolize_keys)
+        next unless topic.changed_for_autosave?
+
+        puts TopicChanges.call(topic) # rubocop:disable Rails/Output
+        topic.save!
+      end
+    end
+  end
+end
+
+Seeds.seed
