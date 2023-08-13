19d9b97985ce6978b044260857f6adb463b40195
John Carney <john@carney.id.au>
2023-08-13 23:04:13 +0000

    Annotate MatchedTerm

--- app/models/matched_term.rb
+++ app/models/matched_term.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: matched_terms
+#
+#  id           :bigint           not null, primary key
+#  text_id      :bigint           not null
+#  term_id      :bigint           not null
+#  matched_text :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class MatchedTerm < ApplicationRecord
   belongs_to :text, inverse_of: :matched_terms
   belongs_to :term, inverse_of: :matched_terms
--- spec/models/matched_term_spec.rb
+++ spec/models/matched_term_spec.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: matched_terms
+#
+#  id           :bigint           not null, primary key
+#  text_id      :bigint           not null
+#  term_id      :bigint           not null
+#  matched_text :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require "rails_helper"
 
 RSpec.describe MatchedTerm, type: :model do
